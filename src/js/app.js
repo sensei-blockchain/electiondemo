15f4002d22f2f4d98b3ffce49107de1e19beffd9
sensei-blockchain <blockchaindev02@gmail.com>
2018-10-09 22:44:54 +0530

    Updated the default web3 port

--- src/js/app.js
+++ src/js/app.js
@@ -10,11 +10,9 @@ App = {
 
   initWeb3: function () {
     if (typeof web3 !== 'undefined') {
-      // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
     } else {
-      // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
     }
     web3 = new Web3(App.web3Provider);
     return App.initContract();
@@ -22,9 +20,7 @@ App = {
 
   initContract: function () {
     $.getJSON("Election.json", function (election) {
-      // Instantiate a new truffle contract from the artifact
       App.contracts.Election = TruffleContract(election);
-      // Connect provider to interact with contract
       App.contracts.Election.setProvider(App.web3Provider);
 
       App.listenForEvents();
@@ -33,18 +29,13 @@ App = {
     });
   },
 
-  // Listen for events emitted from the contract
   listenForEvents: async function () {
     const instance = await App.contracts.Election.deployed();
-    // Restart Chrome if you are unable to receive this event
-    // This is a known issue with Metamask
-    // https://github.com/MetaMask/metamask-extension/issues/2393
     instance.allEvents({}, {
       fromBlock: 0,
       toBlock: 'latest'
     }).watch(function (error, event) {
       console.log("event triggered", event)
-      // Reload when a new vote is recorded
       App.render();
     });
   },
@@ -53,12 +44,12 @@ App = {
     const loader = $("#overlay");
     const content = $("#content");
     const chairpersonContent = $("#chairperson");
+
     $('.vote-switch input').attr('disabled', false)
     loader.show();
     content.hide();
     $("#errorText").hide();
 
-    // Load account data
     web3.eth.getCoinbase(function (err, account) {
       if (err === null) {
         App.account = account;
@@ -67,7 +58,6 @@ App = {
     });
 
     try {
-      // Load contract data
       const instance = await App.contracts.Election.deployed();
       const chairpersonAddress = await instance.chairperson();
       if (App.account !== chairpersonAddress) {
@@ -75,8 +65,6 @@ App = {
       }
       $('#candidatesName').val('');
       $('#voterAddress').val('');
-
-      const candidatesCount = await instance.candidatesCount();
 
       let candidatesResults = $("#candidatesResults");
       let candidatesSelect = $('#candidatesSelect');
@@ -90,15 +78,14 @@ App = {
         $('.vote-status.stopped').addClass('active');
       }
 
+      const candidatesCount = await instance.candidatesCount();
       for (var i = 1; i <= candidatesCount; i++) {
         const candidate = await instance.candidates(i);
         var id = candidate[0];
         var name = candidate[1];
         var voteCount = candidate[3];
 
-        // Render candidate Result
         const candidateTemplate = "<tr id=" + id + "><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
-        // Render candidate ballot option
         const candidateOption = "<option value='" + id + "' >" + name + "</ option>"
 
         if (!($('#candidatesResults tr#' + id).length)) {
@@ -108,13 +95,13 @@ App = {
           $('#candidatesResults tr#' + id).replaceWith(candidateTemplate)
         }
       }
+
       loader.hide();
       content.show();
       $('form').show();
       const voter = await instance.voters(App.account);
       const isVoterRegistered = voter[0];
       const hasVoted = voter[1];
-      // Do not allow a user to vote
       if (!isVoterRegistered) {
         $('#voteForm').hide();
         $('#notAuthorized').show();
