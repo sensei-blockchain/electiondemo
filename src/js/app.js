840b2fb8b320469ff25f1ca8ba03a67df1f899c3
sensei-blockchain <blockchaindev02@gmail.com>
2018-10-10 16:42:02 +0530

    Updated Interface

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,29 @@
 App = {
   web3Provider: null,
   contracts: {},
-  account: '0x0',
+  account: "0x0",
   hasVoted: false,
 
-  init: function () {
+  init: function() {
     return App.initWeb3();
   },
 
-  initWeb3: function () {
-    if (typeof web3 !== 'undefined') {
+  initWeb3: function() {
+    if (typeof web3 !== "undefined") {
+      // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
     } else {
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider(
+        "http://localhost:7545"
+      );
     }
     web3 = new Web3(App.web3Provider);
     return App.initContract();
   },
 
-  initContract: function () {
-    $.getJSON("Election.json", function (election) {
+  initContract: function() {
+    $.getJSON("Election.json", function(election) {
       App.contracts.Election = TruffleContract(election);
       App.contracts.Election.setProvider(App.web3Provider);
 
@@ -29,28 +33,32 @@ App = {
     });
   },
 
-  listenForEvents: async function () {
+  listenForEvents: async function() {
     const instance = await App.contracts.Election.deployed();
-    instance.allEvents({}, {
-      fromBlock: 0,
-      toBlock: 'latest'
-    }).watch(function (error, event) {
-      console.log("event triggered", event)
-      App.render();
-    });
+    instance
+      .allEvents(
+        {},
+        {
+          fromBlock: 0,
+          toBlock: "latest"
+        }
+      )
+      .watch(function(error, event) {
+        console.log("event triggered", event);
+        App.render();
+      });
   },
 
-  render: async function () {
+  render: async function() {
     const loader = $("#overlay");
     const content = $("#content");
-    const chairpersonContent = $("#chairperson");
-
-    $('.vote-switch input').attr('disabled', false)
+    const chairpersonContent = $(".chairperson");
+    $(".vote-switch input").attr("disabled", false);
     loader.show();
     content.hide();
     $("#errorText").hide();
 
-    web3.eth.getCoinbase(function (err, account) {
+    web3.eth.getCoinbase(function(err, account) {
       if (err === null) {
         App.account = account;
         $("#accountAddress").html("Your Account: " + account);
@@ -63,19 +71,19 @@ App = {
       if (App.account !== chairpersonAddress) {
         chairpersonContent.hide();
       }
-      $('#candidatesName').val('');
-      $('#voterAddress').val('');
+      $("#candidatesName").val("");
+      $("#voterAddress").val("");
 
       let candidatesResults = $("#candidatesResults");
-      let candidatesSelect = $('#candidatesSelect');
+      let candidatesSelect = $("#candidatesSelect");
 
       const voteStatus = await instance.votingStarted();
       if (voteStatus) {
-        $('.vote-switch input').attr('checked', true);
-        $('.vote-status.started').addClass('active');
+        $(".vote-switch input").attr("checked", true);
+        $(".vote-status.started").addClass("active");
       } else {
-        $('.vote-switch input').attr('checked', false);
-        $('.vote-status.stopped').addClass('active');
+        $(".vote-switch input").attr("checked", false);
+        $(".vote-status.stopped").addClass("active");
       }
 
       const candidatesCount = await instance.candidatesCount();
@@ -85,100 +93,112 @@ App = {
         var name = candidate[1];
         var voteCount = candidate[3];
 
-        const candidateTemplate = "<tr id=" + id + "><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
-        const candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+        // Render candidate Result
+        const candidateTemplate =
+          "<tr id=" +
+          id +
+          "><th>" +
+          id +
+          "</th><td>" +
+          name +
+          "</td><td>" +
+          voteCount +
+          "</td></tr>";
+        const candidateOption =
+          "<option value='" + id + "' >" + name + "</ option>";
 
-        if (!($('#candidatesResults tr#' + id).length)) {
+        if (!$("#candidatesResults tr#" + id).length) {
           candidatesResults.append(candidateTemplate);
           candidatesSelect.append(candidateOption);
         } else {
-          $('#candidatesResults tr#' + id).replaceWith(candidateTemplate)
+          $("#candidatesResults tr#" + id).replaceWith(candidateTemplate);
         }
       }
 
       loader.hide();
       content.show();
-      $('form').show();
+      $("form").show();
       const voter = await instance.voters(App.account);
       const isVoterRegistered = voter[0];
       const hasVoted = voter[1];
       if (!isVoterRegistered) {
-        $('#voteForm').hide();
-        $('#notAuthorized').show();
+        $("#voteForm").hide();
+        $("#notAuthorized").show();
+      } else if (!voteStatus && App.account !== chairpersonAddress) {
+        $("#voteForm").hide();
+        $("#notStarted").show();
       } else if (hasVoted) {
-        $('#voteForm').hide();
-        $('#alreadyVoted').show();
+        $("#voteForm").hide();
+        $("#alreadyVoted").show();
       } else {
-        $('#notAuthorized').hide();
+        $("#notAuthorized").hide();
       }
-    }
-    catch (error) {
+    } catch (error) {
       console.warn(error);
-    };
+    }
   },
 
-  toggleVoting: async function () {
+  toggleVoting: async function() {
     try {
-      $('.vote-switch input').attr('disabled', true)
+      $(".vote-switch input").attr("disabled", true);
       $("#overlay").show();
-      if ($('.vote-switch input').prop('checked')) {
-        $('.vote-status.stopped').removeClass('active');
-        $('.vote-status.started').addClass('active');
+      if ($(".vote-switch input").prop("checked")) {
+        $(".vote-status.stopped").removeClass("active");
+        $(".vote-status.started").addClass("active");
       } else {
-        $('.vote-status.stopped').addClass('active');
-        $('.vote-status.started').removeClass('active');
+        $(".vote-status.stopped").addClass("active");
+        $(".vote-status.started").removeClass("active");
       }
       const instance = await App.contracts.Election.deployed();
       await instance.toggleVoting({ from: App.account });
     } catch (error) {
-      console.log(error)
+      console.log(error);
       location.reload();
     }
   },
 
-  addCandidate: async function () {
+  addCandidate: async function() {
     try {
       $("#overlay").show();
-      const candidatesName = $('#candidatesName').val();
+      const candidatesName = $("#candidatesName").val();
       const instance = await App.contracts.Election.deployed();
       await instance.addCandidate(candidatesName, { from: App.account });
     } catch (error) {
-      console.log(error)
+      console.log(error);
       App.render();
       $("#errorText").show();
     }
   },
 
-  addVoter: async function () {
+  addVoter: async function() {
     try {
       $("#overlay").show();
-      const voterAddress = $('#voterAddress').val();
+      const voterAddress = $("#voterAddress").val();
       const instance = await App.contracts.Election.deployed();
       await instance.giveRightToVote(voterAddress, { from: App.account });
     } catch (error) {
-      console.log(error)
+      console.log(error);
       App.render();
       $("#errorText").show();
     }
   },
 
-  castVote: async function () {
+  castVote: async function() {
     $("#overlay").show();
-    var candidateId = $('#candidatesSelect').val();
+    var candidateId = $("#candidatesSelect").val();
     try {
-      const instance = await App.contracts.Election.deployed()
+      const instance = await App.contracts.Election.deployed();
       await instance.vote(candidateId, { from: App.account });
-    }
-    catch (err) {
+    } catch (err) {
       console.error(err);
       App.render();
       $("#errorText").show();
-    };
+    }
   }
 };
 
-$(function () {
-  $(window).load(function () {
+$(function() {
+  $(window).load(function() {
     App.init();
   });
 });
