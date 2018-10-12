00062b52a6eccc385321911cdf5f3b33813cae28
sensei-blockchain <blockchaindev02@gmail.com>
2018-10-12 12:57:39 +0530

    Added comments

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,11 +2,22 @@ pragma solidity ^0.4.24;
 
 contract Election {
 
+    /** @dev Defines a Voter struct with
+     * @param registered bool to store the voter's registered status
+     * @param voted bool to represent the voting status
+     */
     struct Voter {
         bool registered;
         bool voted;
     }
 
+
+    /** @dev Defines a Candidate struct with
+     * @param id uint to store the candidates's identifier
+     * @param name string to store the candidates's name
+     * @param registered bool to store the candidate's registered status
+     * @param voteCount uint to store the candidate's vote count
+     */
     struct Candidate {
         uint id;
         string name;
@@ -14,22 +25,39 @@ contract Election {
         uint voteCount;
     }
 
+    // To store the number of candidates
     uint public candidatesCount;
+    // To store the address of the chairperson
     address public chairperson;
+    // To store the status of the election
+    // true if election has started and false if stopped
     bool public votingStarted;
 
+    // Mapping the voter to the unique identifier i.e the address of the voter
     mapping(address => Voter) public voters;
+    // Mapping the candidate to the integer identifier
     mapping(uint => Candidate) public candidates;
 
-    event VotingStatusEvent (bool status);
+    // Events
+    event VotingToggledEvent (bool status);
     event CandidateAddedEvent (uint indexed candidateId);
     event VoterAddedEvent (address indexed voterAddress);
     event VotedEvent (address indexed voterAddress, uint indexed candidateId);
 
+    /**
+     * @dev Constructor
+     * Assign the address of the contract deployer to the chairperson
+     */
     constructor() public {
         chairperson = msg.sender;
     }
 
+    /**
+     * @dev addCandidate, to register a new candidate to the candadates mapping
+     * Can only be called by the chairperson and when the election is in stopped state
+     * Emits the event CandidateAddedEvent
+     * @param candidateName string The name of the candidate
+     */
     function addCandidate(string candidateName) public {
         require(msg.sender == chairperson);
         require(!votingStarted);
@@ -40,6 +68,13 @@ contract Election {
         emit CandidateAddedEvent(candidatesCount);
     }
 
+    /**
+     * @dev giveRightToVote, to authorize a voter
+     * Can only be called by the chairperson and when the election is in stopped state
+     * Throws error if the voter is already registered
+     * Emits the event VoterAddedEvent
+     * @param voterAddress string The address of the voter
+     */
     function giveRightToVote (address voterAddress) public {
         require(msg.sender == chairperson);
         require(!votingStarted);
@@ -49,6 +84,13 @@ contract Election {
         emit VoterAddedEvent(voterAddress);
     }
 
+    /**
+     * @dev vote, to cast a new vote
+     * Can only be called by the registered voter who has not voted yet
+     * Throws error if the voting has not started and if the candidate id is invalid
+     * Emits the event VotedEvent
+     * @param candidateId string The unique identifier of the candidate
+     */
     function vote (uint candidateId) public {
         require(votingStarted);
         require(voters[msg.sender].registered && !voters[msg.sender].voted);
@@ -60,11 +102,16 @@ contract Election {
         emit VotedEvent(msg.sender, candidateId);
     }
 
+    /**
+     * @dev toggleVoting, to toggle the election status
+     * Can only be called by the chairperson
+     * Emits the event VotingToggledEvent
+     */
     function toggleVoting() public {
         require(msg.sender == chairperson);
 
         votingStarted = !votingStarted;
 
-        emit VotingStatusEvent(votingStarted);
+        emit VotingToggledEvent(votingStarted);
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -4,49 +4,43 @@ App = {
   account: "0x0",
   hasVoted: false,
 
-  init: function () {
-    return App.initWeb3();
+  // Start app
+  start: async function () {
+    App.initWeb3();
+    await App.initContract();
+    App.listenForEvents();
+    App.render();
   },
 
+  // Initialize web3
   initWeb3: function () {
     if (typeof web3 !== "undefined") {
       // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
     } else {
       // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider(
-        "http://localhost:7545"
-      );
+      App.web3Provider = new Web3.providers.HttpProvider("http://localhost:7545");
     }
     web3 = new Web3(App.web3Provider);
-    return App.initContract();
   },
 
-  initContract: function () {
-    $.getJSON("Election.json", function (election) {
-      App.contracts.Election = TruffleContract(election);
-      App.contracts.Election.setProvider(App.web3Provider);
-
-      App.listenForEvents();
-
-      return App.render();
-    });
+  // Initialize election contract
+  initContract: async function () {
+    const election = await $.getJSON("Election.json");
+    App.contracts.Election = TruffleContract(election);
+    App.contracts.Election.setProvider(App.web3Provider);
   },
 
+  // Watch events emitted by the contract and re-render
   listenForEvents: async function () {
     const instance = await App.contracts.Election.deployed();
-    instance
-      .allEvents(
-        {},
-        {
-          fromBlock: 0,
-          toBlock: "latest"
-        }
-      )
-      .watch(function (error, event) {
-        console.log("event triggered", event);
-        App.render();
-      });
+    instance.allEvents({}, {
+      fromBlock: 0,
+      toBlock: "latest"
+    }).watch(function (error, event) {
+      console.log("event triggered", event);
+      App.render();
+    });
   },
 
   showLoader: function () {
@@ -66,75 +60,84 @@ App = {
     content.hide();
     $("#errorText").hide();
 
+    // Get the current account address
     web3.eth.getCoinbase(function (err, account) {
       if (err === null) {
         App.account = account;
-        $("#accountAddress").html("Your Account: " + account);
+        $('#accountAddress').html('Your Account: ' + account);
       }
     });
 
     try {
+      // Get an election contract instance
       const instance = await App.contracts.Election.deployed();
-      const chairpersonContent = $(".chairperson");
+      // Get the chairperson address
       const chairpersonAddress = await instance.chairperson();
+
+      const chairpersonContent = $('.chairperson');
       if (App.account !== chairpersonAddress) {
         chairpersonContent.hide();
       }
-      $("#candidatesName").val("");
-      $("#voterAddress").val("");
-
-      let candidatesResults = $("#candidatesResults");
-      let candidatesSelect = $("#candidatesSelect");
 
       const voteStatus = await instance.votingStarted();
       if (voteStatus) {
-        $(".vote-switch input").attr("checked", true);
-        $(".vote-status.started").addClass("active");
+        $('.vote-switch input').attr('checked', true);
+        $('.vote-status.started').addClass('active');
       } else {
-        $(".vote-switch input").attr("checked", false);
-        $(".vote-status.stopped").addClass("active");
+        $('.vote-switch input').attr('checked', false);
+        $('.vote-status.stopped').addClass('active');
       }
 
+      $('#candidatesName').val('');
+      $('#voterAddress').val('');
+
+      // Load the candidates list
+      let candidatesResults = $('#candidatesResults');
+      let candidatesSelect = $('#candidatesSelect');
+      // Get candidates count
       const candidatesCount = await instance.candidatesCount();
       for (var i = 1; i <= candidatesCount; i++) {
+        // Get the candidate data on ith index
         const candidate = await instance.candidates(i);
         var id = candidate[0];
         var name = candidate[1];
         var voteCount = candidate[3];
 
-        // Render candidate Result
-        const candidateTemplate =
-          "<tr id=" + id + "><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>";
-        const candidateOption =
-          "<option value='" + id + "' >" + name + "</ option>";
-
-        if (!$("#candidatesResults tr#" + id).length) {
+        // Render candidate result
+        const candidateTemplate = '<tr id=' + id + '><th>' + id + '</th><td>' + name + '</td><td>' + voteCount + '</td></tr>';
+        const candidateOption = "<option value='" + id + "' >" + name + '</ option>';
+        if (!$('#candidatesResults tr#' + id).length) {
           candidatesResults.append(candidateTemplate);
           candidatesSelect.append(candidateOption);
         } else {
-          $("#candidatesResults tr#" + id).replaceWith(candidateTemplate);
+          $('#candidatesResults tr#' + id).replaceWith(candidateTemplate);
         }
       }
 
       App.hideLoader();
       content.show();
-      $("form").show();
+      $('form').show();
+
+      // Get voter data for the current account
       const voter = await instance.voters(App.account);
+      // Check if current account is authorized to vote
       const isVoterRegistered = voter[0];
+      // Check if current account has already voted
       const hasVoted = voter[1];
+
       if (isVoterRegistered && voteStatus && !hasVoted) {
-        $("#voteForm").show();
+        $('#voteForm').show();
       } else {
-        $("#voteForm").hide();
+        $('#voteForm').hide();
       }
       if (!isVoterRegistered) {
-        $("#notAuthorized").show();
+        $('#notAuthorized').show();
       } else if (!voteStatus && App.account !== chairpersonAddress) {
-        $("#notStarted").show();
+        $('#notStarted').show();
       } else if (hasVoted) {
-        $("#alreadyVoted").show();
+        $('#alreadyVoted').show();
       } else {
-        $("#notAuthorized").hide();
+        $('#notAuthorized').hide();
       }
     } catch (error) {
       console.warn(error);
@@ -143,16 +146,17 @@ App = {
 
   toggleVoting: async function () {
     try {
-      $(".vote-switch input").attr("disabled", true);
+      $('.vote-switch input').attr('disabled', true);
       App.showLoader();
-      if ($(".vote-switch input").prop("checked")) {
-        $(".vote-status.stopped").removeClass("active");
-        $(".vote-status.started").addClass("active");
+      if ($('.vote-switch input').prop('checked')) {
+        $('.vote-status.stopped').removeClass('active');
+        $('.vote-status.started').addClass('active');
       } else {
-        $(".vote-status.stopped").addClass("active");
-        $(".vote-status.started").removeClass("active");
+        $('.vote-status.stopped').addClass('active');
+        $('.vote-status.started').removeClass('active');
       }
       const instance = await App.contracts.Election.deployed();
+      // Call the contract function for toggling the vote status
       await instance.toggleVoting({ from: App.account });
     } catch (error) {
       console.log(error);
@@ -163,45 +167,48 @@ App = {
   addCandidate: async function () {
     try {
       App.showLoader();
-      const candidatesName = $("#candidatesName").val();
+      const candidatesName = $('#candidatesName').val();
       const instance = await App.contracts.Election.deployed();
+      // Call the contract function to add new candidate
       await instance.addCandidate(candidatesName, { from: App.account });
     } catch (error) {
       console.log(error);
       App.render();
-      $("#errorText").show();
+      $('#errorText').show();
     }
   },
 
   addVoter: async function () {
     try {
       App.showLoader();
-      const voterAddress = $("#voterAddress").val();
+      const voterAddress = $('#voterAddress').val();
       const instance = await App.contracts.Election.deployed();
+      // Call the contract function to authorize voter
       await instance.giveRightToVote(voterAddress, { from: App.account });
     } catch (error) {
       console.log(error);
       App.render();
-      $("#errorText").show();
+      $('#errorText').show();
     }
   },
 
   castVote: async function () {
     App.showLoader();
-    var candidateId = $("#candidatesSelect").val();
+    var candidateId = $('#candidatesSelect').val();
     try {
       const instance = await App.contracts.Election.deployed();
+      // Call contract function to cast new vote
       await instance.vote(candidateId, { from: App.account });
     } catch (err) {
       console.error(err);
       App.render();
-      $("#errorText").show();
+      $('#errorText').show();
     }
   }
 };
 
 $(function () {
   $(window).load(function () {
-    App.init();
+    App.start();
   });
 });
